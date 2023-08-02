dcd6a8092d600175915cc9e3a78b98bce2c9c70d
El-Bob <83518564+hobsterizer@users.noreply.github.com>
2023-08-03 02:00:38 +0300

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="about">About</RouterLink>
+  </div>
+  <RouterView/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
 
---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>ahmed ehab</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Home',
+}
+</script>
